5f25ead8bbcfc611880836935dd794280dc35c77
Alfajri <alfajri607@gmail.com>
2025-04-20 17:35:35 +0700

    update

--- src/component/Landing/Content.jsx
+++ src/component/Landing/Content.jsx
@@ -4,7 +4,7 @@ function LandingContent() {
     return(
         <>
             <div>
-                <h2>Berita Sepakbola terkini <a href="http://httpforever.com">klik for my demo project gobola</a></h2>
+                <h2>Berita Sepakbola terkini</h2>
                 <div style={{marginBottom:"1rem"}}></div>
                 <News />
             </div>
