f4fb1cc5d5e84781cc3a5176dc28510280fcf5cd
Alfajri <alfajri607@gmail.com>
2025-04-20 11:22:00 +0700

    update

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -52,7 +52,7 @@ export function Navbar() {
                         paddingTop: "0rem",
                         position: ""
                     }} className={"row"}>
-                        <div className={"col-md-8"} style={{fontSize:"2rem",marginLeft: "1rem"}}>Logo</div>
+                        <div className={"col-md-8"} style={{fontSize:"2rem",Left: '1rem',marginLeft: '1rem',marginRight: '-1rem'}}>Logo</div>
                         <div className={"col-md-4 searchbox"} style={{}}>
                         <ReactSearchBox placeholder="Pertandingan Sepak Bola"
                                     data={[
